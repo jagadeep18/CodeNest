f2b85baa68367467b47630071af41c5dd31837cf
jagadeep_18 <gorantla.jagadeep@gmail.com>
2026-01-22 20:15:31 +0530

    Docker setup and backend config updates

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,7 +16,10 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors())
+app.use(cors({
+	origin: ["http://localhost:5173", "http://localhost:3000", /.*\.ngrok(?:-free)?\.app$/],
+	credentials: true
+}))
 
 // Connect to MongoDB
 connectDB()
